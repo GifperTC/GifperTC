54bfd575c897b0d08f0978b3dc3ab36dfdfacb2e
Nutta Sittipongpanich <59831047+GifperTC@users.noreply.github.com>
2020-02-09 01:03:32 +0700

    9 02 2020

--- codingbat/fuckmylife.java
+++ codingbat/fuckmylife.java
@@ -1,8 +1,26 @@
 public class fuckmylife{
+    public boolean makeBricks(int small, int big, int goal) {
+        int maxBig = goal/5;
+          if(maxBig <= big)
+              goal -= maxBig*5;
+          else
+              goal -= big*5;
+          if(goal <= small)
+              return true;
+          return false;
+    }
     public int loneSum(int a, int b, int c) {
-        int[] sum;
-        for (int i : sum) {
-            sum
+        if(a == b){
+           if(a == c)
+                return 0;
+            else
+                return c; 
         }
-      }
+        if(a == c)
+            return b;
+        if(b == c)
+            return a;
+        return a + b + c;
+    }
+    
 }
